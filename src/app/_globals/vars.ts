0a0ea1275e7ee2bdd5e99381ea3d9886c82ea4f1
skandermez <meziane.skander@outlook.fr>
2019-12-07 12:25:42 +0100

    create bet-sheet-details component

--- src/app/_globals/vars.ts
+++ src/app/_globals/vars.ts
@@ -1,7 +1,7 @@
 import {environment} from '../../environments/environment';
 
 const ENABLE_CROSS_ORIGIN_URL = 'https://cors-anywhere.herokuapp.com/';
-export const API_KEY = '2muxiU0WSzkhMc6Z';
-export const API_SECRET = 'ASaNNeiYJySF5Hw741FmVGjOfD52Qc2r';
+export const API_KEY = 'cV1EMCB5mBfw8xgw';
+export const API_SECRET = 'Z4oweQ3kTuq2nnIzjJe5mPbIrrezZv6Y';
 export const LIVE_SCORES_URL =  ENABLE_CROSS_ORIGIN_URL + 'livescore-api.com/api-client/scores/live.json';
 
---
+++ src/app/bet-sheet-details/bet-sheet-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bet-sheet-details',
+  templateUrl: './bet-sheet-details.component.html',
+  styleUrls: ['./bet-sheet-details.component.css']
+})
+export class BetSheetDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
