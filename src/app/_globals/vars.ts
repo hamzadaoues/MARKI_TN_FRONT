779f9c115d79ed136e1f2406b4989a7a5b56dd90
skandermez <meziane.skander@outlook.fr>
2019-12-25 23:58:03 +0100

    d

--- src/app/_globals/vars.ts
+++ src/app/_globals/vars.ts
@@ -1,7 +1,7 @@
 import {environment} from '../../environments/environment';
 
 const ENABLE_CROSS_ORIGIN_URL = 'https://cors-anywhere.herokuapp.com/';
-export const API_KEY = '2muxiU0WSzkhMc6Z';
-export const API_SECRET = 'ASaNNeiYJySF5Hw741FmVGjOfD52Qc2r';
+export const API_KEY = 'JU42k7Y0Ot4Y67io';
+export const API_SECRET = 'wAqMP7pxnCwrULeDFjG0qF0f85eecwCJ';
 export const LIVE_SCORES_URL =  ENABLE_CROSS_ORIGIN_URL + 'livescore-api.com/api-client/scores/live.json';
 
